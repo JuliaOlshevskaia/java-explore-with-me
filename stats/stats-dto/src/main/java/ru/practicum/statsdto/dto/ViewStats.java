1abd4ff86810fc8c57134bf0912404ef5c1bf0ef
18973107 <YVOlshevskaya@sberbank.ru>
2024-09-09 18:29:59 +0300

    fix: codestyle

--- stats/stats-dto/src/main/java/ru/practicum/statsdto/dto/ViewStats.java
+++ stats/stats-dto/src/main/java/ru/practicum/statsdto/dto/ViewStats.java
@@ -16,13 +16,11 @@ public class ViewStats implements Comparable {
         Integer p1 = this.getHits();
         Integer p2 = that.getHits();
 
-        if (p1 > p2 ){
+        if (p1 > p2) {
             return -1;
-        }
-        else if (p1 < p2){
+        } else if (p1 < p2) {
             return 1;
-        }
-        else
+        } else
             return 0;
     }
 }
